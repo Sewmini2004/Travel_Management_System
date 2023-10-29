7dcd8a793b3a896ca64e814bd655cacb532a4c19
sewmini2004 <sewminipremodya98@gmail.com>
2023-10-30 03:59:20 +0530

    Created the Guide Serice Validations

--- Services/guideService/src/main/java/lk/ijse/springboot/guideService/entity/Guide.java
+++ Services/guideService/src/main/java/lk/ijse/springboot/guideService/entity/Guide.java
@@ -1,31 +1,48 @@
 package lk.ijse.springboot.guideService.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import jakarta.validation.constraints.*;
+import lombok.*;
 
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
 @ToString
 @Entity
 
 public class Guide implements Super {
     @Id
+    @GeneratedValue
     private String guideId;
+    @NotNull(message = "Guide name shouldn't be null")
+    @Pattern(regexp = "^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$" , message ="Invalid guide name")
     private String guideName;
+    @NotNull(message = "Guide address shouldn't be null ")
+    @Pattern(regexp ="^[a-zA-Z0-9\\\\s,.'-]+[a-zA-Z0-9\\\\s,.'-]+[a-zA-Z0-9\\\\s,.'-]+$"
+    , message = "Invalid  guide address")
     private String guideAddress;
-    private String address;
-    private byte[] guideIdImage;
+    @NotEmpty(message = "Guide's Id images shouldn't be empty")
+    private String guideIdImage;
+    @NotEmpty(message = "Gender shouldn't be empty")
     private String gender;
+    @NotBlank(message = "Contact No shouldn't be blank")
+    @Pattern(regexp ="^(\\+\\d{1,2}\\s)?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}$" , message = "Invalid contact number")
     private String contactNumber;
-    private byte[] nicImage;
+    @NotEmpty(message = "Guide's NIC Images FrontEnd shouldn't be empty")
+    private String nicImageFrontEnd;
+    @NotEmpty(message = "Guide's NIC Images BackEnd shouldn't be empty")
+    private String nicImageBackEnd;
+    @NotEmpty(message = "Experience shouldn't be empty")
     private String Experience;
+    @NotEmpty(message = "Man day value shouldn't be empty")
+    @Negative(message = " Man day value shouldn't be empty")
+    @Min(value = 0, message = "Man day value shouldn't be less than 0")
     private int manDayValue;
+    @NotEmpty(message = "Remarks shouldn't be empty")
     private String remarks;
 
 }
