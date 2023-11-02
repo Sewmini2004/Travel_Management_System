498551791da4e0b346b4a874153345c459891474
sewmini2004 <sewminipremodya98@gmail.com>
2023-11-02 14:17:10 +0530

    Updated  the Exception

--- Services/travelService/src/main/java/lk/ijse/springboot/travelService/repository/TravelRepo.java
+++ Services/travelService/src/main/java/lk/ijse/springboot/travelService/repository/TravelRepo.java
@@ -3,6 +3,6 @@ package lk.ijse.springboot.travelService.repository;
 import lk.ijse.springboot.travelService.entity.Travel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TravelRepo extends JpaRepository<Travel,String> {
+public interface TravelRepo extends JpaRepository<Travel,Long> {
 
 }
